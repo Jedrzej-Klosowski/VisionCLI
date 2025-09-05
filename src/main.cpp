7749ecab0db6bf3ebd3b7f4e3a6c862106e14c55
Jedrzej Klosowski <andre48681@gmail.com>
2025-09-05 15:17:01 +0200

    Add main application file and update CMakeLists.txt to reflect new source path

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+
+int main() {
+    std::cout << "Vision CLI is running!" << std::endl;
+    cv::Mat img = cv::imread(R"(../assets/dog.jpg)", cv::IMREAD_COLOR);
+    if (img.empty()) {
+        std::cerr << "Could not open or find the image!" << std::endl;
+        return -1;
+    }
+
+
+    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Display Image", img);
+    cv::waitKey(0);
+
+    cv::destroyAllWindows();
+
+    return 0;
+}
