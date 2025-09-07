83c8066a98b5250707ba9f223b613e723832ff6c
Jedrzej Klosowski <andre48681@gmail.com>
2025-09-07 20:03:20 +0200

    Enhance main application to support image input via command line arguments using CLI11

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,30 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
-#include <../include/CLI11.hpp>
+#include "../include/CLI11.hpp"
 
 #include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
     std::cout << "Vision CLI is running!" << std::endl;
-    cv::Mat img = cv::imread(R"(../assets/dog.jpg)", cv::IMREAD_COLOR);
+
+    CLI::App app{"Vision CLI"};
+    std::string image_path;
+    app.add_option("-i,--image", image_path, "Path to the image file")
+       ->required()
+       ->check(CLI::ExistingFile);
+
+    CLI11_PARSE(app, argc, argv);   // 🔥 to uruchamia parsowanie argumentów
+
+    cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
     if (img.empty()) {
         std::cerr << "Could not open or find the image!" << std::endl;
         return -1;
     }
 
-
     cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
     cv::imshow("Display Image", img);
     cv::waitKey(0);
-
     cv::destroyAllWindows();
 
     return 0;
